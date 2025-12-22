4de67c56fa5ec46e95845a3b4f76eec30462739a
Fetkullin Yrii <02011994jkl@mail.ru>
2025-12-22 16:55:08 +0300

    Initial Commit

---
+++ PizzaShop/PizzaShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaShopApp.swift
+//  PizzaShop
+//
+//  Created by Юрий Феткуллин on 22.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct PizzaShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
