bfd85832894abb4f1a77fb7032747dd8a55cd51b
Fetkullin Yrii <02011994jkl@mail.ru>
2025-12-23 16:09:30 +0300

    Анимация и изменение состояний

--- PizzaShop/ContentView.swift
+++ PizzaShop/ContentView.swift
@@ -9,17 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
 
+    @State private var isAuth = true
     @State private var email = ""
     @State private var password = ""
+    @State private var condirmPassword = ""
 
     var body: some View {
         VStack(spacing: 20) {
-            Text("Авторизация")
-                .padding()
+            Text(isAuth ? "Авторизация" : "Регистрация")
+                .padding(isAuth ? 16 : 24)
                 .padding(.horizontal, 30)
                 .font(.title2.bold())
                 .background(Color("whiteAlpha"))
-                .cornerRadius(10)
+                .cornerRadius(isAuth ? 30 : 60)
 
             VStack {
                 TextField("Введите e-mail", text: $email)
@@ -36,10 +38,23 @@ struct ContentView: View {
                     .padding(8)
                     .padding(.horizontal, 12)
 
+                if !isAuth {
+                    SecureField("Повторите ваш пароль", text: $condirmPassword)
+                        .padding()
+                        .background(Color("whiteAlpha"))
+                        .cornerRadius(12)
+                        .padding(8)
+                        .padding(.horizontal, 12)
+                }
+
                 Button {
-                    print("Авторизация")
+                    if isAuth {
+                        print("Авторизация")
+                    } else {
+                        print("Регистрация")
+                    }
                 } label: {
-                    Text("Войти")
+                    Text(isAuth ? "Войти" : "Создать аккаунт")
                         .padding()
                         .frame(maxWidth: .infinity)
                         .background(LinearGradient(colors: [Color("myYellow"), Color("myOrange")], startPoint: .leading, endPoint: .trailing))
@@ -51,9 +66,9 @@ struct ContentView: View {
                 }
 
                 Button {
-                    print("Ещё не с нами?")
+                    isAuth.toggle()
                 } label: {
-                    Text("Ещё не с нами?")
+                    Text(isAuth ? "Ещё не с нами?" : "Уже есть аккаунт!")
                         .padding(.horizontal)
                         .frame(maxWidth: .infinity)
                         .cornerRadius(12)
@@ -67,10 +82,14 @@ struct ContentView: View {
             .padding(.top, 20)
             .background(Color("whiteAlpha"))
             .cornerRadius(24)
-            .padding(30)
+            .padding(isAuth ? 30 : 12)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Image("bg").ignoresSafeArea())
+        .background(Image("bg")
+            .ignoresSafeArea()
+            .blur(radius: isAuth ? 0 : 6)
+        )
+        .animation(Animation.easeInOut(duration: 0.2), value: isAuth)
     }
 }
 
